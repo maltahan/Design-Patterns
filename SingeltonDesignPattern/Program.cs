e76004d9bff3373fc007914c1a7387d70a77070f
Mohammad <mohammad.altahan@sharedop.com>
2017-06-20 16:06:53 +0200

    Add Singelton DesignPattern to the project

---
+++ SingeltonDesignPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SingeltonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // illegal construct
+            //Compile Time Error: The constructor SingleObject() is not visible
+            //SingleObject object = new SingleObject();
+
+            //Get the only object available
+            SingeltonObject SingeltonObj = SingeltonObject.getInstance();
+
+            SingeltonObj.Id = 1;
+            SingeltonObj.Name = "Mohammad";
+
+            //show the message
+            SingeltonObj.showMessage();
+
+            //Get another but it takes the same values as the first one .
+
+            SingeltonObject SingeltonObj1 = SingeltonObject.getInstance();
+
+            Console.WriteLine(SingeltonObj.Id + "" + SingeltonObj.Name);
+
+            Console.WriteLine(SingeltonObj1.Id + "" + SingeltonObj1.Name);
+        }
+    }
+}
---
+++ SingeltonDesignPattern/SingeltonObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SingeltonDesignPattern
+{
+    public class SingeltonObject
+    {
+        //create an object of SingleObject
+        private static SingeltonObject instance = new SingeltonObject();
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        //make the constructor private so that this class cannot be
+        //instantiated
+        private SingeltonObject() { }
+
+        //Get the only object available
+        public static SingeltonObject getInstance()
+        {
+            return instance;
+        }
+
+        public void showMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
